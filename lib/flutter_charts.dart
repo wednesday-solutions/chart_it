0eccaacfb55367b872d3980044f9d1f6677dbdb6
Rishabh <rushabh.more@wednesday.is>
2023-02-22 17:52:25 +0530

    Added PlaceHolder Chart for testing painter

--- lib/flutter_charts.dart
+++ lib/flutter_charts.dart
@@ -1,3 +1,4 @@
 library flutter_charts;
 
 // TODO: All Public APIs should be exported here!
+export 'src/charts/temp/placeholder_chart.dart';
---
+++ lib/src/charts/temp/placeholder_chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class PlaceHolderChart extends StatefulWidget {
+  const PlaceHolderChart({Key? key}) : super(key: key);
+
+  @override
+  State<PlaceHolderChart> createState() => _PlaceHolderChartState();
+}
+
+class _PlaceHolderChartState extends State<PlaceHolderChart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

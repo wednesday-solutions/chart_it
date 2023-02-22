0ae53e243def4ec51047c92012551c05a0ba1200
Rishabh <rushabh.more@wednesday.is>
2023-02-22 13:16:29 +0530

    Updated library file for exports

--- lib/flutter_charts.dart
+++ lib/flutter_charts.dart
@@ -1,7 +1,3 @@
 library flutter_charts;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// TODO: All Public APIs should be exported here!
